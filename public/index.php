b430a04f3d8896f81d5e4c9d2ee05934375091b6
Dominik Łukasik <dmcode@outlook.com>
2022-08-18 17:22:10 +0200

    Konfiguracja sesji oraz twiga

--- public/index.php
+++ public/index.php
@@ -2,14 +2,37 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Middleware\Session;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$twig = Twig::create(__DIR__.'/../templates', ['cache' => false]);
+
+$app->add(
+    new Session([
+        'name' => 'SESSID',
+        'autorefresh' => true,
+        'lifetime' => '20 minutes',
+        'httponly' => true
+    ])
+);
+
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'layout.html.twig', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
 
 $app->run();

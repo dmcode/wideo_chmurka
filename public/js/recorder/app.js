6fee6db12770323bc2136eb42005c4ca6f6192a3
Dominik Łukasik <dmcode@outlook.com>
2022-08-19 16:50:30 +0200

    Podłaczenie zdarzeń do mediacontrols

--- public/js/recorder/app.js
+++ public/js/recorder/app.js
@@ -19,17 +19,64 @@ const selectMedia = async () => {
 }
 
 
+const startRecording = ({elMediaControls=null, elapsedTag=null, btnRec=null}) => {
+  recorder.reset();
+  recorder.onTimeInterval(seconds => {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds - (min * 60);
+    elapsedTag.innerText = `${min < 10 ? '0' + min : min}:${(sec < 10 ? '0' + sec : sec)}`;
+  });
+  recorder.onStart(_ => {
+    const tmplRecordingInfo = cloneTemplate('tmplRecordingInfo');
+    if (tmplRecordingInfo && elMediaPreview) {
+      elMediaPreview.querySelector('.recording-info').appendChild(tmplRecordingInfo);
+    }
+  });
+  recorder.onStop(_ => {
+    btnRec.checked = false;
+    if (elMediaPreview)
+      elMediaPreview.querySelector('.recording-info').replaceChildren();
+  });
+  recorder.start();
+}
+
+
+const stopRecording = () => {
+  if (recorder)
+    recorder.stop();
+}
+
+
+const renderMediaControls = () => {
+  const elMediaControls = cloneTemplate('tmplMediaControls');
+  if (!elMediaControls)
+    return;
+  const btnRec = elMediaControls.getElementById('btnRec');
+  const elapsedTag = elMediaControls.getElementById('elapsed');
+  btnRec.addEventListener('change', e => {
+    if (e.target.checked)
+      startRecording({elMediaControls, elapsedTag, btnRec});
+    else
+      stopRecording();
+  });
+  elMediaPreview.appendChild(elMediaControls);
+}
+
+
 const renderSelectMedia = () => {
   const elSelectMedia = cloneTemplate('tmplSelectMedia');
   if (!elSelectMedia)
     return;
-  const btn = elSelectMedia.querySelector('.select-media');
+  const btn = elSelectMedia.querySelector('.btn-select-media');
   btn.addEventListener('click', async e => {
     e.preventDefault();
     e.stopPropagation();
     await selectMedia();
-    if (hasMediaSelected())
+    if (hasMediaSelected()) {
       btn.remove();
+      elMediaPreview.querySelector('.select-media').remove();
+      renderMediaControls();
+    }
   });
   elMediaPreview.appendChild(elSelectMedia);
 }
--- public/js/recorder/recorder.js
+++ public/js/recorder/recorder.js
@@ -6,9 +6,21 @@ class VideoRecorder {
     constructor({media, videoTag=null, mimeType="video/webm"}) {
         this._mimeType = mimeType;
         this._chunks = [];
+        this._timeLimit = 10;
+        this._startTime = 0;
+        this._endTime = 0;
+        this._timeInterval = null;
+        this._timeIntervalHandlers = [];
+        this._onStartHandlers = [];
+        this._onStopHandlers = [];
+
         this._recorder = new MediaRecorder(media, {mimeType});
         this._recorder.addEventListener('dataavailable', e => {
             this._chunks.push(e.data);
+            const myStream = this._recorder.stream;
+        });
+        this._recorder.addEventListener('start', () => {
+            this._onStartHandlers.forEach(callback => callback());
         });
         this._recorder.addEventListener('stop', () => {
              const blob = new Blob(this._chunks, {
@@ -16,18 +28,62 @@ class VideoRecorder {
              });
              const url = URL.createObjectURL(blob);
              console.log(url);
-        })
+            this._onStopHandlers.forEach(callback => callback());
+        });
 
         if (videoTag)
             videoTag.srcObject = media;
     }
 
+    reset() {
+        this._chunks = [];
+        this._startTime = 0;
+        this._endTime = 0;
+        this._timeInterval = null;
+        this._timeIntervalHandlers = [];
+        this._onStartHandlers = [];
+        this._onStopHandlers = [];
+    }
+
     start() {
         this._recorder.start(50);
+        this._startTime = new Date();
+        this._timeInterval = setInterval(_ => {
+            const seconds = this.#timeDiff();
+            this._timeIntervalHandlers.forEach(callback => callback(seconds));
+            if (seconds >= this._timeLimit)
+                this.stop();
+        }, 1000);
     }
 
     stop() {
+        this._endTime = new Date();
         this._recorder.stop();
+        clearInterval(this._timeInterval);
+    }
+
+    onStart(callback) {
+        if (typeof callback === "function")
+            this._onStartHandlers.push(callback)
+    }
+
+    onStop(callback) {
+        if (typeof callback === "function")
+            this._onStopHandlers.push(callback)
+    }
+
+    onTimeInterval(callback) {
+        if (typeof callback === "function")
+            this._timeIntervalHandlers.push(callback);
+    }
+
+    /**
+     * Zwraca liczbę sekund, które upłyneły od rozpoczęcia nagrywania
+     * @returns {number}
+     */
+    #timeDiff() {
+        const timeDiff = new Date() - this._startTime;
+        return Math.round(timeDiff/1000);
     }
 
     save() {

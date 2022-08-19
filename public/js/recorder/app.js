25b1e6325ef79c48428c21b4bd9e846347868102
Dominik Łukasik <dmcode@outlook.com>
2022-08-19 11:29:48 +0200

    Przycisk wyboru źródła wideo

--- public/js/recorder/app.js
+++ public/js/recorder/app.js
@@ -2,6 +2,7 @@ import {hasMediaSelected, getDisplayMedia, getVideoRecorder} from "./recorder.js
 import {cloneTemplate} from "../tools.js";
 
 const elVideoRecorder = document.querySelector('.video-recorder');
+const elMediaPreview = document.querySelector('.media-preview-wrapper');
 const videoTag = document.getElementById('mediaPreview');
 
 let recorder = null;
@@ -25,11 +26,12 @@ const renderSelectMedia = () => {
   const btn = elSelectMedia.querySelector('.select-media');
   btn.addEventListener('click', async e => {
     e.preventDefault();
+    e.stopPropagation();
     await selectMedia();
     if (hasMediaSelected())
       btn.remove();
   });
-  elVideoRecorder.appendChild(elSelectMedia);
+  elMediaPreview.appendChild(elSelectMedia);
 }
 
 

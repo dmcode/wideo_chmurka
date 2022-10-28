bbce90562cc6fbf112a77ed2e6f84a806772c82e
Dominik Łukasik <dmcode@outlook.com>
2022-10-28 19:19:54 +0200

    Zainicjowanie backendu na Laravel 9

--- public/index.php
+++ public/index.php
@@ -1,163 +1,55 @@
 <?php
-use Controllers\AuthController;
-use Controllers\PublicController;
-use Controllers\LibraryController;
-use Controllers\StreamController;
-use DI\Container;
-use Middleware\AuthMiddleware;
-use Middleware\LoginRequired;
-use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Log\LoggerInterface;
-use Services\AuthService;
-use Services\DatabaseService;
-use Services\LibraryService;
-use Services\StorageService;
-use Services\VideoService;
-use Slim\Exception\HttpNotFoundException;
-use Slim\Factory\AppFactory;
-use Slim\Middleware\Session;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-use SlimSession\Helper;
-use Twig\AppExtension;
 
-require __DIR__ . '/../vendor/autoload.php';
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-define('APP_ROOT', dirname(__DIR__));
+define('LARAVEL_START', microtime(true));
 
-$dotenv = Dotenv\Dotenv::createImmutable(APP_ROOT);
-$dotenv->load();
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-$container = new Container();
-$app = AppFactory::createFromContainer($container);
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-$errorMiddleware->setDefaultErrorHandler(function (
-    ServerRequestInterface $request,
-    Throwable $exception,
-    bool $displayErrorDetails,
-    bool $logErrors,
-    bool $logErrorDetails,
-    ?LoggerInterface $logger = null
-) use ($app) {
-    $response = $app->getResponseFactory()->createResponse();
-    $response = $response->withStatus(500);
-    if ($exception instanceof HttpNotFoundException)
-        $response = $response->withStatus($exception->getCode());
-    $response->getBody()->write($exception->getMessage());
-    return $response;
-});
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-$app->add((function () use ($container) {
-    return new AuthMiddleware($container);
-})());
+require __DIR__.'/../vendor/autoload.php';
 
-$app->add(
-    new Session([
-        'name' => 'SESSID',
-        'autorefresh' => true,
-        'lifetime' => '20 minutes',
-        'httponly' => true
-    ])
-);
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$container->set('session', function () {
-    return new Helper();
-});
+$kernel = $app->make(Kernel::class);
 
-$container->set('route.parser', function (ContainerInterface $container) use ($app) {
-    return $app->getRouteCollector()->getRouteParser();
-});
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-$container->set('view', function (ContainerInterface $container) {
-    $twig = Twig::create(APP_ROOT.'/templates', ['cache' => false]);
-    $twig->getEnvironment()->addExtension(new AppExtension($container));
-    return $twig;
-});
-$app->add(TwigMiddleware::createFromContainer($app));
-
-
-// Services
-$container->set('db', function (ContainerInterface $container) {
-    return new DatabaseService($container);
-});
-
-$container->set('auth', function (ContainerInterface $container) {
-    return new AuthService($container);
-});
-
-$container->set('storage', function (ContainerInterface $container) {
-    return new StorageService($container);
-});
-
-$container->set('video', function (ContainerInterface $container) {
-    return new VideoService($container);
-});
-
-$container->set('library', function (ContainerInterface $container) {
-    return new LibraryService($container);
-});
-
-
-// Controllers
-$container->set('PublicController', function (ContainerInterface $container) {
-    return new PublicController($container);
-});
-
-$container->set('AuthController', function (ContainerInterface $container) {
-    return new AuthController($container);
-});
-
-$container->set('LibraryController', function (ContainerInterface $container) {
-    return new LibraryController($container);
-});
-
-$container->set('StreamController', function (ContainerInterface $container) {
-    return new StreamController($container);
-});
-
-
-// Routing
-$app->get('/', 'PublicController:index')->setName('index');
-
-$app->get('/login', 'AuthController:login')->setName('login');
-$app->post('/login', 'AuthController:loginSubmit')->setName('login_submit');
-
-$app->get('/logout', 'AuthController:logout')->setName('logout');
-
-$app->get('/singup', 'AuthController:singup')->setName('singup');
-$app->post('/singup', 'AuthController:singupSubmit')->setName('singup_submit');
-
-$app->get('/library', 'LibraryController:index')
-    ->add((function() use ($container) { return new LoginRequired($container); })())
-    ->setName('library');
-
-$app->get('/library/{video_slug}', 'LibraryController:video')
-    ->add((function() use ($container) { return new LoginRequired($container); })())
-    ->setName('library_video');
-
-$app->get('/stream/{video_slug}', 'StreamController:video')
-    ->setName('stream_video');
-
-$app->post('/api/view/{video_slug}', 'LibraryController:registerVideoView')
-    ->setName('register_view');
-
-$app->post('/api/upload_blob', 'LibraryController:uploadBlobVideo')
-    ->add((function() use ($container) { return new LoginRequired($container); })())
-    ->setName('upload_blob');
-
-$app->post('/api/video_data', 'LibraryController:updateVideoData')
-    ->add((function() use ($container) { return new LoginRequired($container); })())
-    ->setName('video_data');
-
-$app->get('/thumb/{thumb_id}', 'StreamController:thumb')
-    ->setName('stream_thumb');
-
-$app->get('/{video_slug}', 'PublicController:video')
-    ->setName('public_video');
-
-$app->addBodyParsingMiddleware();
-$app->run();
+$kernel->terminate($request, $response);
--- public/js/tools.js
+++
@@ -1,6 +0,0 @@
-export const cloneTemplate = (elementId) => {
-  const tmpl = document.getElementById(elementId);
-  if (!tmpl)
-    return null;
-  return tmpl.content.cloneNode(true);
-}
